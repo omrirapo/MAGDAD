4eef20b486bc53405f680838687c9478386a133e
omrirapo <119509281+omrirapo@users.noreply.github.com>
2023-03-23 11:36:31 +0200

    Initial commit

---
+++ Mouth_Detecting_Module/detecting_edges.py
@@ -0,0 +1,16 @@
+import cv2
+
+img = cv2.imread("image.jpg")
+#Display original image
+cv2.imshow('Original', img)
+cv2.waitKey(0)
+
+# Convert to graycsale
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Blur the image for better edge detection
+img_blur = cv2.GaussianBlur(img_gray, (3, 3), 0)
+# Canny Edge Detection
+edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200)  # Canny Edge Detection
+# Display Canny Edge Detection Image
+cv2.imshow('Canny Edge Detection', edges)
+cv2.waitKey(0)
