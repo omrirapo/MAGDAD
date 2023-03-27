addfed79e22213c03f5c2a08c3294889e37b0a90
omrirapo <119509281+omrirapo@users.noreply.github.com>
2023-03-27 10:57:01 +0300

    sucseeded finding pedri's mouth!

--- Mouth_Detecting_Module/mouth_detection_example.py
+++ Mouth_Detecting_Module/mouth_detection_example.py
@@ -1,13 +1,13 @@
 import cv2
 
 # Load the cascade
-mouth_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+mouth_cascade = cv2.CascadeClassifier('haarcascade_mcs_mouth.xml')
 # Read the input image
 img = cv2.imread('pedri_img.jpg')
 # Convert into grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Detect faces
-mouthes = mouth_cascade.detectMultiScale(gray, 1.1, 500)
+mouthes = mouth_cascade.detectMultiScale(gray, 1.1, 200)
 # Draw rectangle around the faces
 for (x, y, w, h) in mouthes:
     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
