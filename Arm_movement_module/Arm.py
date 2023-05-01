3d1eaf41cb228327c996cb39848da5e37f9ae739
omrirapo <119509281+omrirapo@users.noreply.github.com>
2023-05-01 15:55:46 +0300

    added arm and motor classes' watch out' continuous motor class is no good!

---
+++ Arm_movement_module/Arm.py
@@ -0,0 +1,9 @@
+class Arm:
+
+    def __init__(self, WristMotor, ArmMotor, ShoulderMotor):
+        self._WristMotor = WristMotor
+        self._ArmMotor = ArmMotor
+        self._ShoulderMotor = ShoulderMotor
+        s
+
+
---
+++ Arm_movement_module/Motor.py
@@ -0,0 +1,43 @@
+import typing
+from typing import Callable
+
+from gpiozero import Servo
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+
+class Motor:
+    def __init__(self, GPIO: int, Anlge2ValConverter : Callable, MinSignal = 0.5 / 1000, MaxSignal = 2.5 / 1000 ):
+        factory = PiGPIOFactory()
+        self._servo = Servo(GPIO, min_pulse_width= MinSignal, max_pulse_width= MaxSignal, pin_factory=factory)
+        self._Anlge2ValConverter =Anlge2ValConverter
+        self.currAngle = 0
+        self.move_to_angle(self.currAngle)
+
+    @property
+    def Anlge2ValConverter(self):
+        return self._Anlge2ValConverter
+
+    @Anlge2ValConverter.setter
+    def Anlge2ValConverter(self, value):
+        self._Anlge2ValConverter = value
+
+    @property
+    def servo(self):
+        return self._servo
+
+    @servo.setter
+    def servo(self, value):
+        self._servo = value
+
+    def move_to_angle(self, new_angle):
+        self._servo.value = self._Anlge2ValConverter(new_angle)
+        self.currAngle = new_angle
+
+    def get_angle(self):
+        return self.currAngle
+
+
+
+
+
+
