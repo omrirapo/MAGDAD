00d6c99f1293e87bc2d1291d022190fa444570ac
eyalweintroub <eyal.weintroub@mail.huji.ac.il>
2023-05-23 17:07:49 +0300

    update to previous run control

--- Mouth_Detecting_Module/run_control.py
+++ Mouth_Detecting_Module/run_control.py
@@ -5,12 +5,15 @@ from pid import PID
 from only_mouth import mouthing
 from time import sleep
 import pickle
+import RPi.GPIO as GPIO
 
 
 def orient(name="user", cam_height=0):
+    print("started orient")
+
     arm_motor = Motor(17, lambda alpha: alpha / 90)
     wrist_motor = Motor(18, lambda alpha: alpha / 90)
-    shoulder_motor = None
+    shoulder_motor = StepperMotor(20, 21, 200, 131.34)
     arm = Arm(arm_motor, wrist_motor, shoulder_motor, 89.142, 129.5)
     arm.move_hand_by_motors_input(0, -90, -90)
 
@@ -28,8 +31,13 @@ def orient(name="user", cam_height=0):
         while val is not None:
             arm.move_hand_by_motors_input(0, arm.get_alpha1() + val * 10, arm.get_alpha1() + val * 10)
             t += 1
-            print(val)
             val = mouther()
+
+    sleep(0.5)
+    arm.move_hand(arm.get_x(), arm.get_y() + 35, 0)
+
+    print("finished orient")
+
     # db[name] = arm.get_alpha1()
 
     # with open('DB', 'ab') as dbfile:
@@ -38,7 +46,35 @@ def orient(name="user", cam_height=0):
 
 
 #     pickle.dump(db, dbfile, protocol=pickle.HIGHEST_PROTOCOL)
+def move_till_touch(should_i_stop):
+    print("started move")
+
+    # Define the GPIO pin connected to the touch sensor output
+    TOUCH_SENSOR_PIN = 2
+
+    # Initialize the GPIO library
+    GPIO.setmode(GPIO.BCM)
+
+    # Set the touch sensor pin as an input with a pull-up resistor
+    GPIO.setup(TOUCH_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    # Loop forever to detect touch
+    shoulder_motor = StepperMotor(16, 21, 200, 131.34)
+
+    count = 0
+    while True:
+
+        # Check if the touch sensor pin is grounded, indicating a touch has been detected
+        if GPIO.input(TOUCH_SENSOR_PIN):
+            count += 1
+            print(f"Touch Detected! :{count}")
+            break
+            # Wait for a short period to prevent multiple touch detections
+        for i in range(4):
+            shoulder_motor.step()
+    print("finished_move")
 
 
 if __name__ == '__main__':
-    orient("eyal")
+    orient("")
+    move_till_touch(lambda: False)
--- Mouth_Detecting_Module/stepper_motor.py
+++ Mouth_Detecting_Module/stepper_motor.py
@@ -3,26 +3,56 @@ from typing import Callable
 
 from gpiozero import Servo
 from gpiozero.pins.pigpio import PiGPIOFactory
+from time import sleep
+import RPi.GPIO as GPIO
+
+DELAY = 0.00324
+CW = 1
+CCW = 0
 
 
 class StepperMotor:
-    def __init__(self, GPIO: int, steps_per_rotation: int, radius: int = None):
+    def __init__(self, DIR: int, STEP: int, steps_per_rotation: int, circ: int = None):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(DIR, GPIO.OUT)
+        GPIO.setup(STEP, GPIO.OUT)
+        GPIO.output(DIR, CCW)
+
+        self._DIR = DIR
+        self._STEP = STEP
         self._steps_per_rotation = steps_per_rotation
-        self._radius = radius
+        self._circ = circ
         self.angle = 0
 
+    def step(self):
+        GPIO.output(self._STEP, GPIO.HIGH)
+        sleep(DELAY)
+        GPIO.output(self._STEP, GPIO.LOW)
+        sleep(DELAY)
+
     def move_to_angle(self, new_angle):
+        rotations = (new_angle - self.angle) / 360
+        steps = round(self._steps_per_rotation * rotations)
+        if rotations > 0:
+            GPIO.output(self._DIR, CW)
+
+        else:
+            GPIO.output(self._DIR, CCW)
+            steps *= -1
+        for i in range(steps):
+            self.step()
+
         self.angle = new_angle
 
     def get_angle(self):
         return self.angle
 
     def move_to_x(self, new_x):
-        if self._radius is None:
+        if self._circ is None:
             raise Exception("radius is None")
-        self.angle = new_x / self._radius
+        self.move_to_angle(360 * ((new_x - self.get_x()) / self._circ))
 
     def get_x(self):
-        if self._radius is None:
+        if self._circ is None:
             raise Exception("radius is None")
-        return self.angle * self._radius
+        return self.angle / 360 * self._circ
